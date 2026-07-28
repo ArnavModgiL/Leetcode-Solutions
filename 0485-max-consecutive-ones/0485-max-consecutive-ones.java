977f97f4820e6b3c7ae68c0d4c85439888c027ff
ARNAV-MODGIL <arnavmodgil08@gmail.com>
2026-07-29 00:49:28 +0530

    Time: 2 ms (98.31%), Space: 52.5 MB (66.56%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -3,7 +3,7 @@ class Solution {
         int count = 0;
         int max = 0;
 
-        for(int i = 0; i<nums.length;i++){
+        for(int i = 0; i < nums.length;i++){
             if(nums[i] == 1){
                 count++;
             } else {
@@ -11,7 +11,6 @@ class Solution {
             } if(count > max){
                 max = count;
             }
-        }
-        return max;
+        } return max;
     }
 }
