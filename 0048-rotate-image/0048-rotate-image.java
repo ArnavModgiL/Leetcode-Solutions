7a0a53d29ccd6e8a281ac128b7b7042f1af04881
ARNAV-MODGIL <arnavmodgil08@gmail.com>
2026-07-28 18:16:16 +0530

    Time: 0 ms (100%), Space: 43.6 MB (71.84%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,15 +1,13 @@
 class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
-        // find trnaspose !
-        for(int i = 0;i < n;i++){
+        for(int i = 0; i < n;i++){
             for(int j = i + 1; j < n; j++){
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
             }
         }
-        // reverse Matrix :
         for(int i = 0; i<n;i++){
             int left = 0;
             int right = n - 1;
