73e6662d52ccb22c21f4a145e4b7265db28c7765
ARNAV-MODGIL <arnavmodgil08@gmail.com>
2026-08-02 16:51:14 +0530

    Time: 1 ms (97.41%), Space: 47.5 MB (81.55%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -13,9 +13,10 @@ class Solution {
                     col[j] = true;
                 }
             }
-        } for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
-                if(row[i] || col[j]){
+        }
+        for(int i = 0; i<rows;i++){
+            for(int j = 0; j<cols;j++){
+                if(row[i] || col[j]) {
                     matrix[i][j] = 0;
                 }
             }
