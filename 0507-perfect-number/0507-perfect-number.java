a7333ee6c535604e18ec34a110c1ef53cabf31a6
ARNAV-MODGIL <arnavmodgil08@gmail.com>
2026-08-17 16:33:51 +0530

    Time: 0 ms (100%), Space: 42.2 MB (26.31%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -1,15 +1,8 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
-        int sum = 0;
-        for(int i = 1; i < num-1; i++) {
-            if(num%i == 0) {
-                sum += i;
-            }
-        }
-        if(sum == num) {
+        if(num == 6 || num == 28 || num == 496 || num == 8128 || num == 33550336) {
             return true;
-        } else {
-            return false;
         }
+        return false;
     }
 }
