33fdd52ea928e6ba4bbd6aa599cb4ff343b1586e
ARNAV-MODGIL <arnavmodgil08@gmail.com>
2026-08-30 16:04:52 +0530

    Time: 0 ms (100%), Space: 44.5 MB (25.57%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -10,33 +10,33 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if(head == null || head.next == null || k == 0){
+        if(head == null || head.next == null || k == 0){ // aghr head = 0 yha fir head kie aaghe element nhi hai ya fir joo k hai voo zero hai soo .
             return head;
         }
+        int length = 0; // length variable bniye.
+        ListNode current = head; // current node bniye use head ki or point out kr diya okie!
+        ListNode tail = null; // current tail node bniye use null ki equal kr diya ji.
 
-        ListNode current = head;
-        ListNode tail = null;
-        int length = 0;
-        while(current != null){
-            length++;
+        while(current != null){ // jbh tkh current null kiye equal nhi hota 
+            length++; // length increase krdoo ++ 
             if(current.next == null){
-                tail = current;
+                tail = current; // aghr current ke aagye null hai to tail ko current bndoo okie
             }
-            current = current.next;
+            current = current.next; // current pointer ko current.next pointer pr move krdoo
         }
-
-        k = k % length;
+        k = k % length; 
         if(k == 0){
             return head;
         }
-        tail.next = head;
-        ListNode newTail = head;
+        tail.next = head; // list ko basically rotate kr diya hmne!
+        ListNode newTail = head; // new Tail bniye joo ki head sai start hogyi. . .
         for(int i = 0; i < length - k - 1; i++){
             newTail = newTail.next;
         }
-        ListNode newHead = newTail.next;
-        newTail.next = null;
+        ListNode newHead = newTail.next; // ikh new head bniye joo ki new tail ki aggye sai start ooo.
+        newTail.next = null; // new tail koo null ki equal kr dye.
 
-        return newHead;
+
+        return newHead; // new head ko last mai return krdiye;
     }
 }
