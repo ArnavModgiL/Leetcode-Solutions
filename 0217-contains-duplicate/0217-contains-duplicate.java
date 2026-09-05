7f7618baf3140fc34bd3a862d0c8cb7a4c49e54e
ARNAV-MODGIL <arnavmodgil08@gmail.com>
2026-09-05 21:54:17 +0530

    Time: 13 ms (98.84%), Space: 108.7 MB (19.11%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,14 +1,14 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         int x = nums.length;
-        if(x < 2){
+        if(x < 2){ // agr 2 sai km hai to return false.
             return false;
         }
         Set<Integer> set = new HashSet<Integer>();
-
+        
         for(int i = 0; i < x; i++){
-            if(!set.add(nums[i])){
-                return true;
+            if(!set.add(nums[i])){ // agar element set mein already present tha. . .
+                return true; // toh duplicate mil gaya true return kroo.
             }
         }
         return false;
