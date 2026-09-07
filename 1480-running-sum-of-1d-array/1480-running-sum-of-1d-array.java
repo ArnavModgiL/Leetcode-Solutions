82385f42110dc007c5ff46c7b0bb94fd8d77384e
ARNAV-MODGIL <arnavmodgil08@gmail.com>
2026-09-07 16:50:23 +0530

    Time: 0 ms (100%), Space: 44.3 MB (54.07%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -1,8 +1,9 @@
 class Solution {
     public int[] runningSum(int[] nums) {
-        for(int i = 1; i < nums.length; i++){
+        for(int i = 1; i < nums.length; i++){ // start from 1 bez i[0] sai phle element nhi hai.
+            // current element + previous ka running sum -> current position ki value. . .
             nums[i] = nums[i] + nums[i - 1];
         }
-        return nums;
+        return nums; // nums ki value return krdo.
     }
 }
