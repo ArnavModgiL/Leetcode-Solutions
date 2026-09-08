ccef551dde725b8d3f9e1765dbbbd24c9f0d86cc
ARNAV-MODGIL <arnavmodgil08@gmail.com>
2026-09-08 07:19:02 +0530

    Time: 1 ms (83.65%), Space: 42.7 MB (39.85%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,5 +1,8 @@
 class Solution {
     public int countCommas(int n) {
-        return Math.max(0, n - 999);
+        if(n < 1000){
+            return 0; // Aghr number jo hai 1000 sai chotu hai soo no comma islye return 0.
+        }
+        return n - 999; // isme [last - first + 1] condition used hai okie. . .
     }
 }
